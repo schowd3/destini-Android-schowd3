23932900cf61dd2f6c08db68fc987c03388ba0f1
siam-al-merchowdhury <schowd3@gmu.edu>
2020-01-25 13:44:54 -0500

    SemiDone Check for button2 followed by button2.

--- app/src/main/java/com/londonappbrewery/destini/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/destini/MainActivity.java
@@ -2,11 +2,18 @@ package com.londonappbrewery.destini;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    // TODO: Steps 4 & 8 - Declare member variables here:
+    // member variables
 
+    private TextView storyTextView;
+    private Button storyTopButton;
+    private Button storyBottomButton;
+    private int storyIndex=1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,12 +21,57 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        // TODO: Step 5 - Wire up the 3 views from the layout to the member variables:
+        //Initialised/Assigned variables
+        storyTextView=findViewById(R.id.storyTextView);
+        storyTopButton=findViewById(R.id.buttonTop);
+        storyBottomButton=findViewById(R.id.buttonBottom);
 
 
 
-        // TODO: Steps 6, 7, & 9 - Set a listener on the top button:
+        //  listener on the top, continues the story from the top button(T3)
+        storyTopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(storyIndex==1) {
+                    storyTextView.setText(R.string.T3_Story);
+                    //updates the button
+                    storyTopButton.setText(R.string.T3_Ans1);
+                    storyBottomButton.setText(R.string.T3_Ans2);
+                    storyIndex=3;
+                }else {
+                    storyTextView.setText(R.string.T6_End);
+                    storyTopButton.setVisibility(View.GONE);
+                    storyBottomButton.setVisibility(View.GONE);
 
+                }
+            }
+        });
+
+        //listener on bottombutton, continues the story from the bottombutton(T2)
+        storyBottomButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(storyIndex==1||storyIndex==2){
+                storyTextView.setText(R.string.T2_Story);
+                //updates the button
+                storyTopButton.setText(R.string.T2_Ans1);
+                storyBottomButton.setText(R.string.T2_Ans2);
+                storyIndex=2;
+                } //second part of the story
+                else if(storyIndex==2){
+                    storyTextView.setText(R.string.T4_End);
+                    //Story finishes, T4_endpoint
+                    storyTopButton.setVisibility(View.GONE);
+                    storyBottomButton.setVisibility(View.GONE);
+                }
+                else{
+                    storyTextView.setText(R.string.T4_End);
+                    storyTopButton.setVisibility(View.GONE);
+                    storyBottomButton.setVisibility(View.GONE);
+                }
+            }
+
+        });
 
 
 
